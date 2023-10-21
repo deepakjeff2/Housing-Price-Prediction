e9c9a471215063d09163277049340d2268558c43
deepak k <deepakjeff2@gmail.com>
2023-10-21 22:28:50 +0530

    web app is ready

--- app.py
+++ app.py
@@ -24,3 +24,13 @@ def predict_api():
     output = regression.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict',methods =['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    new_data = scaler.transform(np.array(data).reshape(1,-1))
+    output = regression.predict(new_data)[0]
+    return render_template('home.html',prediction_text="The house price prediction is {}".format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
